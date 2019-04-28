1f5d6335c6da8b294b3431abae26c22b66b89d72
jonnrauber <natanrauber97@gmail.com>
2019-04-28 16:10:25 -0300

    testing environment with partial game deploy

---
+++ digital-games/assets/tileset-office.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Tileset" tilewidth="16" tileheight="16" tilecount="360" columns="30">
+ <image source="images/tileset_office.png" width="480" height="192"/>
+</tileset>
